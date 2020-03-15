fd191d2e49323d6c8e6705804b4bf08fd863ade9
ab14at <ab14at@my.fsu.edu>
2020-03-15 00:45:02 -0400

    fixing formatting

--- Hw02.java
+++ Hw02.java
@@ -1,148 +1,322 @@
 import java.io.*;
 import java.util.*;
 
-public class Hw02{
+public class Hw02 {
+    private Node negative;
+    private Node positive;
 
-    public static class node{
-        //public String key; //what is this for
-        public Integer value;
+    public Hw02(){
+        negative = new Node(Integer.MIN_VALUE);
+        positive = new Node(Integer.MAX_VALUE);
 
-        public node up;
-        public node down;
-        public node left;
-        public node right;
-
-        //helper infinity declarations
-        public static int negative = Integer.MIN_VALUE;
-        public static int positive = Integer.MAX_VALUE;
+        negative.setRight(positive);
+        positive.setLeft(negative);
     }
 
-    public static class skiplist{
-        public node head;
-        public node tail;
+    public static class Node{
+        public int value;
+        public Node left;
+        public Node right;
+        public Node up;
+        public Node down;
 
-        public int n; //number of entries in list
-        public int h; //height 
-        public Random r; //coin toss
-
-        public static void SkipList(){
-            node tmp, tmp2;
-
-            tmp = new node(node.negative, null);
-            tmp2 = new node(node.positive, null);
-
-            head = tmp;
-            tail = tmp2;
-
-            tmp.right = tmp2;
-            tmp2.left = tmp;
-
-            n = 0; //no entries to start
-            h = 0; //initial height 0
-
-            r = new Random(42)%2;
+        public Node(int value){
+            this.value = value;
+            this.left = null;
+            this.right = null;
+            this.up = null;
+            this.down = null;
         }
-//SEARCHING FUNCTIONS
-        public node searching(int value){
-            node tmp;
-            tmp = head;
 
-            while(true){
-                while((tmp.right.value) != node.positive && (tmp.right.value) <= value){
-                    tmp = tmp.right;
-                }
-                //try going down a level
-                if(tmp.down != null){
-                    tmp = tmp.down;
-                }
-                else{
-                    break;
-                }
+        public Node(Node tmp){
+            this.value = tmp.value;
+            this.left = null;
+            this.right = null;
+            this.up = null;
+            this.down = tmp;
+        }
+
+        public Node getLeft(){
+            return left;
+        }
+        public void setLeft(Node left){
+            this.left = left;
+        }
+        
+        public Node getRight() {
+            return right;
+        }
+        public void setRight(Node right) {
+            this.right = right;
+        }
+
+        public Node getUp() {
+            return up;
+        }
+        public void setUp(Node up) {       
+            this.up = up;     
+        }
+
+        public Node getDown() {       
+            return down;
+        }
+        public void setDown(Node down) {      
+            this.down = down;        
+        }
+
+    }//end node class
+
+    public Node search(int value){
+        Node current = negative;
+
+        while(current != null){
+
+            while(current.right != null && current.right.value <= value){
+                current = current.right;
             }
-            return(tmp);
-        }
 
-        public Integer search(int value){
-            node tmp;
-            tmp = searching(value);
-
-            if(tmp.value == value){
-                 return(tmp.value);
-            }
-            else{
-                System.out.println("not found");
-                return(null);
-            }
-        }
-
-        public void addLayer(){
-            node tmp, tmp2;
-            tmp = new node(node.negative, null);
-            tmp2 = new node(node.positive, null);
-
-            tmp.right = tmp2;
-            tmp.down = head;
-
-            tmp2.left = tmp;
-            tmp.down = tail;
-
-            head.up = tmp;
-            tail.up = tmp2;
-
-            head = tmp;
-            tail = tmp2;
-
-            h += 1; //add to height count
-        }
-
-//INSERTING FUNCTION
-        public Integer insert(int value){
-            node tmp, tmp2;
-            int i;
-
-            tmp = searching(value);
-
-            if(tmp.value == value){ //does the value already exist in the list
+            if(current.value == value){
                 break;
             }
-            else{ //it does not, enter it
-                tmp2 = new node(value);
 
-                tmp2.left = tmp;
-                tmp2.right = tmp.right;
-                tmp.right.left = tmp2;
-                tmp.right = tmp2;
+            current = current.down;
+        }
+        return current;
+    }
 
-                i=0;
-                while(r.nextDouble() < 0.5){
-                    if(i >= h){
-                        addLayer();
-                    }
+    public boolean insert(int value){
+        Node current = negative; 
+        List<Node> updatedlist = new ArrayList<Node>();
 
-                    while(tmp.up == null){
-                        tmp = tmp.left;
-                    }
-                    tmp = tmp.up;
+        while(current != null){
 
-                    node tmp3;
-
-                    tmp3 = new node(value, null);
-                    tmp3.left = tmp;
-                    tmp3.right = tmp.right;
-                    tmp3.down = tmp2;
-
-                    tmp.tight.left = tmp3;
-                    tmp.right = tmp3;
-                    tmp2.up = tmp3;
-
-                    tmp2 = tmp3;
-
-                    i += 1;
-                }
-                n += 1; //another entry was added to the list
-                return(null);
+            while(current.right != null && current.right.value < value){
+                current = current.right;
             }
+
+            updatedlist.add(current);
+            current = current.down;
+        }
+
+        int level = 0;
+        Node tmp = null;
+
+        while(level == 0 || coinToss()){
+
+            if(tmp == null){
+                tmp = new Node(value);
+            }
+            else{
+                tmp = new Node(tmp);
+            }
+
+            Node tmp2 = null;
+
+            if(updatedlist.size() <= level){
+                newLevel();
+                tmp2 = this.negative;
+            }
+            else{
+                tmp2 = updatedlist.get(updatedlist.size() - level - 1);
+            }
+
+            tmp.right = tmp2.right;
+            tmp.left = tmp2;
+            tmp.right.left = tmp;
+            tmp2.right = tmp;
+
+            level++;
+        }
+        return true;
+    }
+
+    public boolean delete(int value){
+        Node current = this.negative;
+        List<Node> updatedlist = new ArrayList<Node>();
+
+        while(current != null){
+
+            while(current.right != null && current.right.value < value){
+                current = current.right;
+            }
+
+            if(current.right.value == value){
+                updatedlist.add(current);
+            }
+            current = current.down;
+        }
+
+        for(int i=0; i<updatedlist.size(); i++){
+            Node update = updatedlist.get(i);
+            Node delete = update.right;
+
+            update.right = delete.right;
+            delete.right.left = update;
+            delete.up = null;
+            delete.down = null;
+        }
+        return true;
+    }
+
+    public void newLevel(){
+        Node negative = new Node(Integer.MIN_VALUE);
+        Node positive = new Node(Integer.MAX_VALUE);
+
+        negative.setRight(positive);
+        positive.setLeft(negative);
+
+        this.negative.up = negative;
+        negative.down = this.negative;
+        this.negative = negative;
+
+        this.positive.up = positive;
+        positive.down = this.positive;
+        this.positive = positive;
+    }
+
+    public boolean coinToss(){
+        // Random r = new Random();
+        // long seed = 42;
+        // r.setSeed(seed);
+        // return r.nextBoolean();
+        return Math.random() >= 0.5;
+    }
+
+    public void print(){
+        Node current = this.negative;
+
+        while(current.down != null){
+            current = current.down;
+        }
+
+        current = current.right;
+
+        while(current.right != null){
+            System.out.print(current.value + " ");
+            current = current.right;
+        }
+        System.out.println();
+    }
+
+    public void printAll(){
+        Node current = this.negative;
+
+        while(current != null){
+            Node first = current;
+            first = first.right;
+
+            while(first.right != null){
+                System.out.print(first.value + " ");
+                first = first.right;
+            }
+
+            current = current.down;
+            System.out.println();
         }
     }
 
+    public static void complexityIndicator(){
+        System.out.println("al144291;4;15");
+    }
+
+    public static void main(String[] args){
+
+        Hw02 list = new Hw02();
+        boolean result = false;
+        String substr = "";
+
+        String infile = args[0];
+
+        try{
+            complexityIndicator();
+            System.out.println("For the input file named " + infile);
+            System.out.println("With the RNG unseeded,");
+            System.out.println("the current Skip List is shown below:");
+            System.out.println("---infinity");
+
+            BufferedReader br = new BufferedReader(new FileReader(infile));
+            // StringBuilder sb = new StringBuilder();
+            // String line = br.readLine();
+
+            // while(line != null){
+            //     sb.append(line);
+            //     sb.append(System.lineSeparator());
+            //     line = br.readLine();
+            // }
+
+            // String everything = sb.toString();
+            // String[] input = everything.split("\n");
+
+        
+
+            // for(int i=0; i<input.length; i++){
+            //     String tmp = input[i];
+            // String line = "";
+            // while((line = br.readLine()) != null){
+
+            //     if(tmp.startsWith("i")){
+            //         substr=tmp.substring(2,4);
+            //         result = list.insert(Integer.parseInt(substr)); 
+            //     }
+            //     else if(tmp.startsWith("s")){
+            //         Node node = list.search(Integer.parseInt(tmp.substring(2,4)));
+
+            //         if(node!=null){
+            //             System.out.println(tmp.substring(2,5) + " found");
+            //         }
+            //         else{
+            //             System.out.println(tmp.substring(2,5) + " not found");
+            //         }
+            //     }
+            //     else if(tmp.startsWith("d")){
+            //         result = list.delete(Integer.parseInt(tmp.substring(2,4)));
+
+            //         if(result == true){
+            //             System.out.println(tmp.substring(2,5) + " deleted");
+            //         }
+            //         else{
+            //             System.out.println(tmp.substring(2,5) + " not deleted");
+            //         }
+            //     }
+            Scanner scnr = new Scanner(new File(infile));
+
+            char command = 'o';
+
+            while(scnr.hasNext()){
+                command = scnr.next().charAt(0);
+
+                if(command == 'i'){
+                    list.insert(scnr.nextInt());
+                }
+                else if(command == 's'){
+                    list.search(scnr.nextInt());
+                    if(list.search(scnr.nextInt()) != null){
+                        System.out.print("found");
+                    }else{
+                        System.out.println("not found");
+                    }
+                }
+                else if(command == 'd'){
+                    list.delete(scnr.nextInt());
+                    if(list.delete(scnr.nextInt()) != false){
+                        System.out.print("deleted");
+                    }else{
+                        System.out.println("not deleted");
+                    }
+                }
+                else if(command == 'p'){
+                    list.printAll();
+                }
+                // else if (tmp.startsWith("p")){
+                //     list.printAll();
+                // }
+            }//end of while
+            System.out.println("+++infinity");
+            System.out.print("---End of Skip List---");
+        }//end try
+
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }//end main
 }
---
+++ rls2020sp/hw02test.sh
@@ -0,0 +1,24 @@
+#compile
+javac Hw02.java
+#begin test steps
+java Hw02 H2in-a1.txt >a1St.txt
+diff a1St.txt Valid-a1St.txt
+java Hw02 H2in-a2.txt >a2St.txt
+diff a2St.txt Valid-a2St.txt
+java Hw02 H2in-a3.txt >a3St.txt
+diff a3St.txt Valid-a3St.txt
+java Hw02 H2in-a4.txt >a4St.txt
+diff a4St.txt Valid-a4St.txt
+java Hw02 H2in-a5.txt >a5St.txt
+diff a5St.txt Valid-a5St.txt
+java Hw02 H2in-a6.txt >a6St.txt
+diff a6St.txt Valid-a6St.txt
+java Hw02 H2in-a7.txt >a7St.txt
+diff a7St.txt Valid-a7St.txt
+java Hw02 H2in-a8.txt >a8St.txt
+diff a8St.txt Valid-a8St.txt
+java Hw02 H2in-a9.txt >a9St.txt
+diff a9St.txt Valid-a9St.txt
+java Hw02 H2in-a10.txt >a10St.txt
+diff a10St.txt Valid-a10St.txt
+
